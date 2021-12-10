ce0b629bcdd808887a230dfa5cfda143f982c29a
MonicaSelvaraj <mselvaraj@wisc.edu>
2021-12-09 20:46:56 -0500

    got the index and coordinate of closest granule

--- granuleTranslation.py
+++ granuleTranslation.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import csv
+from scipy.spatial import distance
 from scipy.spatial.distance import cdist
 
 def getPoints(filename):
@@ -29,17 +30,20 @@ C1C3centers = np.array(C1C3centers, dtype = float)
 #print(C1C3centers)
  
 #STEP 2 - Find the granule point that each center point is closest to and make a plot of distances. 
-closestC2s = list()
+closestC2s = list() #List of the closest granule point
+minindex = list() #List of closest granule index
+#mindist = list() #List of distances between granule points and center points
 for c in C1C3centers:
-    closestC2s.append(arr2[cdist([c], arr2).argmin()])
+    curindex = cdist([c], arr2).argmin()
+    minindex.append(curindex)
+    #mindist.append(distance.curindex)
+    closestC2s.append(arr2[curindex])
+minindex = np.array(minindex, dtype = int)
+#mindist = np.array(mindist, dtype = float)
 closestC2s = np.array(closestC2s, dtype = float)
+print(minindex)
+#print(mindist)
 print(closestC2s)
  
-
-#D = spdist.cdist(, verts)
-
-
-#def closest_granule(node, nodes):
-#    return nodes[cdist([node], nodes).argmin()]
 
 
